8651a6b90470df802303d41fb7b72d2df235c92e
Nat Martin <nmartin@rochester.rr.com>
2015-02-18 18:55:56 +0530

    Pushing the first version of the testing lesson.

---
+++ html/examples/blog/spec/blog-spec.js
@@ -0,0 +1,10 @@
+describe("blog", function() {
+
+	it("defines a function called addBlog() to be called by the post button.", function () {
+		expect(addBlog).toBeDefined();
+	});
+
+	it("defines a new blog post", function () {
+
+	});
+});

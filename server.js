0813fdb5b34bf4da1e3c0824a252c01049cdedd6
Nat Martin <nmartin@rochester.rr.com>
2015-02-24 10:04:08 +0530

    Putting the new version of the file and slides up. Working on testing server.

--- server.js
+++ server.js
@@ -1,28 +1,4 @@
-var fs = require('fs');
 var http = require('http');
-var url = require('url');
-var ROOT_DIR = "html/";
+var server = require('./lib/server.js');
 
-function returnErrorMessage(result, error) {
-	result.writeHead(404);
-	result.end(JSON.stringify(error));
-};
-
-function returnPage(result, page) {
-	result.writeHead(200);
-	result.end(page);	
-};
-
-function server(request, result) {
-    var urlObj = url.parse(request.url, true, false);
-    fs.readFile(ROOT_DIR + urlObj.pathname, function(error, page) {
-		if (error) {
-			returnErrorMessage(result, error);
-		} else {
-			returnPage(result, page);
-    	}
-    });
-};
-
-http.createServer(server).listen(3030);
-
+http.createServer(server.server).listen(3030);
